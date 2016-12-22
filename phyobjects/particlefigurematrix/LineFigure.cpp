5c58360050a514c160413f6dc116ad5ece87b94b
magistrser <mgistrser@gmail.com>
2016-12-22 11:44:09 +0300

    Folde for figure

---
+++ phyobjects/particlefigurematrix/LineFigure.cpp
@@ -0,0 +1,21 @@
+/*
+ * LineFigure.cpp
+ *
+ *  Created on: Dec 22, 2016
+ *      Author: root
+ */
+
+#include <LineFigure.h>
+
+namespace phycoub {
+
+LineFigure::LineFigure() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LineFigure::~LineFigure() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace phycoub */
---
+++ phyobjects/particlefigurematrix/LineFigure.h
@@ -0,0 +1,21 @@
+/*
+ * LineFigure.h
+ *
+ *  Created on: Dec 22, 2016
+ *      Author: root
+ */
+
+#ifndef LINEFIGURE_H_
+#define LINEFIGURE_H_
+
+namespace phycoub {
+
+class LineFigure {
+public:
+	LineFigure();
+	virtual ~LineFigure();
+};
+
+} /* namespace phycoub */
+
+#endif /* LINEFIGURE_H_ */
