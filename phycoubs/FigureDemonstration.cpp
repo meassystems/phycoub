828838124a534b1545e1f4ee5c8087a64f2a527f
magistrser <mgistrser@gmail.com>
2016-12-22 13:08:33 +0300

    LineFigure Demo added

---
+++ phycoubs/FigureDemonstration.cpp
@@ -0,0 +1,36 @@
+/*
+ * FigureDemonstration.cpp
+ *
+ *  Created on: Dec 22, 2016
+ *      Author: root
+ */
+
+#include <FigureDemonstration.h>
+
+#include <algorithm>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+namespace phycoub {
+
+FigureDemonstration::FigureDemonstration() {
+	srand(time(NULL));
+	rand(); rand(); rand();
+
+	createField_.push_back(CreateField(&azotField_, &borderFieldCondition_, "LD NN Field"));
+	feelField_.push_back(FeelField(&highSpeedModificationVerle_, &createField_.back(), &azotInterworking, "LD NN Feel", &dt_));
+
+	createField_.back().addGroupParticle(lineN.particles_);
+	feelField_.back().addGroupParticle(lineN.particles_);
+}
+FigureDemonstration::~FigureDemonstration() {}
+
+void FigureDemonstration::phyCoub() {
+	for_each(feelField_.begin(), feelField_.end(), [](FeelField& feelField_) {
+		feelField_.moveParticles();
+	}
+	);
+}
+
+} /* namespace phycoub */
---
+++ phycoubs/FigureDemonstration.h
@@ -0,0 +1,54 @@
+/*
+ * FigureDemonstration.h
+ *
+ *  Created on: Dec 22, 2016
+ *      Author: root
+ */
+
+#ifndef FIGUREDEMONSTRATION_H_
+#define FIGUREDEMONSTRATION_H_
+
+#include <vector>
+
+#include "Vector.h"
+#include "Particle.h"
+#include "CreateField.h"
+#include "FeelField.h"
+
+#include "ElasticCoubCondition.h"
+#include "BorderFieldCondition.h"
+#include "HighSpeedModificationVerle.h"
+#include "LDFieldFunction.h"
+#include "LDInterworking.h"
+
+#include "LineFigure.h"
+
+namespace phycoub {
+
+class FigureDemonstration {
+public:
+	FigureDemonstration();
+	virtual ~FigureDemonstration();
+
+	void phyCoub();
+
+	double dt_ = 1E-15, k_ = 1.38E-23, z_ = 0.;
+	Vector borders_{1E-8, 1E-8, 1E-8};
+	double mN_ = 23.24E-27, epsN = 95.05*k_, aN = 3.698E-10;
+
+	LineFigure lineN{Vector(0, 0, aN), 10, Vector(1E-9), Vector(0, 1,.0), mN_, z_, &elasticBorder_};
+private:
+	ElasticCoubCondition elasticBorder_{&borders_};
+	BorderFieldCondition borderFieldCondition_{BorderFieldCondition()};
+	HighSpeedModificationVerle highSpeedModificationVerle_{HighSpeedModificationVerle()};
+
+	LDFieldFunction azotField_{aN, aN, epsN};
+	LDInterworking azotInterworking;
+
+	std::vector<CreateField> createField_;
+	std::vector<FeelField> feelField_;
+};
+
+} /* namespace phycoub */
+
+#endif /* FIGUREDEMONSTRATION_H_ */
