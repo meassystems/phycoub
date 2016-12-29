02430b3f2a9b02b2c9815cf13cbc9d0ba5773556
magistrser <mgistrser@gmail.com>
2016-12-29 23:17:18 +0300

    New Arhitectyre, added CalculationGroup

---
+++ phyobjects/CalculationGroup.cpp
@@ -0,0 +1,40 @@
+/*
+ * CalculationGroup.cpp
+ *
+ *  Created on: Dec 29, 2016
+ *      Author: root
+ */
+
+#include <CalculationGroup.h>
+
+#include <algorithm>
+
+namespace phycoub {
+
+CalculationGroup::CalculationGroup(CalculationModel* calculatiomModel, double* dt): calculationModel_(calculatiomModel), dt_(dt) {}
+CalculationGroup::~CalculationGroup() {}
+
+void CalculationGroup::phyModeling() {
+		calculationModel_->phyCalculate(this);
+}
+
+void CalculationGroup::addParticle(Particle* particle) {
+	particles_.push_back(particle);
+}
+void CalculationGroup::addGroupParticle(std::vector<Particle*> &particles) {
+	for_each(particles.begin(), particles.end(), [&](Particle* particle){
+		particles_.push_back(particle);
+	});
+}
+void CalculationGroup::removeParticle(Particle* particle) {
+	std::vector<Particle *>::iterator itr = particles_.begin();
+	while(itr != particles_.end()) {
+		if((*itr)->index == particle->index) {
+			particles_.erase(itr);
+			break;
+		}
+		++itr;
+	}
+}
+
+} /* namespace phycoub */
---
+++ phyobjects/CalculationGroup.h
@@ -0,0 +1,38 @@
+/*
+ * CalculationGroup.h
+ *
+ *  Created on: Dec 29, 2016
+ *      Author: root
+ */
+
+#ifndef CALCULATIONGROUP_H_
+#define CALCULATIONGROUP_H_
+
+#include <vector>
+
+#include "Vector.h"
+#include "Particle.h"
+#include "CalculationModel.h"
+
+namespace phycoub {
+
+class CalculationGroup {
+public:
+	CalculationGroup(CalculationModel* calculatiomModel, double* dt);
+	virtual ~CalculationGroup();
+
+	void phyModeling();
+
+	void addParticle(Particle* particle);
+	void addGroupParticle(std::vector<Particle*> &particles);
+	void removeParticle(Particle* particle);
+
+	std::vector<Particle*> particles_;
+	double* dt_;
+private:
+	CalculationModel* calculationModel_;
+};
+
+} /* namespace phycoub */
+
+#endif /* CALCULATIONGROUP_H_ */
