ff66d81457631049262f79161b6a4fe156472de6
Sergey Frantsishkov <mgistrser@gmail.com>
2020-03-31 10:33:49 +0300

    Generate qmake for widgets

--- generateQMake.sh
+++ generateQMake.sh
@@ -11,22 +11,25 @@ function writeSomthingToVariable {
     #$1 folder
     #$2 variablename
     #$3 somthing CMakeLists
+    #$4 relative path
+    #$5 qmake extension
 
     local projectName=$(basename $1)
-    local qmakePath=$1/$projectName.qmake
+    local qmakePath=$1/$projectName.$5
 
     for somthing in $3
     do
-        echo "$2 += ../$projectName/${somthing#"$1/"}" >> $qmakePath
+        echo "$2 += $4$projectName/${somthing#"$1/"}" >> $qmakePath
     done
     echo >> $qmakePath
 }
 
 function writeWidgetsSpecific {
     #$1 folder
+    #$2 qmake extension
 
     local projectName=$(basename $1)
-    local qmakePath=$1/$projectName.qmake
+    local qmakePath=$1/$projectName.$2
 
     echo -e "LIBS += -L\$\$PWD/../$projectName/ -l$projectName\n" >> $qmakePath
 }
@@ -35,13 +38,28 @@ function generateQMake {
     #$1 folder
 
     local projectName=$(basename $1)
-    local qmakePath=$1/$projectName.qmake
+    local extension="qmake"
+    local qmakePath=$1/$projectName.$extension
 
     rm -r $qmakePath 2> /dev/null
 
     getIncludeDirectories $1 #return includeDirectories
-    writeSomthingToVariable $1 INCLUDEPATH "$includeDirectories"
-    writeWidgetsSpecific $1
+    writeSomthingToVariable $1 INCLUDEPATH "$includeDirectories" "../" $extension
+    writeWidgetsSpecific $1 $extension
+}
+
+function generateQMakeWidgets {
+    #$1 folder
+
+    local projectName=$(basename $1)
+    local extension="widgets.qmake"
+    local qmakePath=$1/$projectName.$extension
+
+    rm -r $qmakePath 2> /dev/null
+
+    getIncludeDirectories $1 #return includeDirectories
+    writeSomthingToVariable $1 INCLUDEPATH "$includeDirectories" "../../" $extension
 }
 
 generateQMake $PWD
+generateQMakeWidgets $PWD
