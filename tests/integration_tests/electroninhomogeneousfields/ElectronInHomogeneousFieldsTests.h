d34037265146e69b05c4401d92696f7aefffd081
Sergey Frantsishkov <mgistrser@gmail.com>
2021-01-02 00:56:57 +0300

    Integration tests

---
+++ tests/integration_tests/electroninhomogeneousfields/ElectronInHomogeneousFieldsTests.h
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+class ElectronInHomogeneousFieldsTests : public ::testing::Test
+{
+  protected:
+    ElectronInHomogeneousFieldsTests() = default;
+    virtual ~ElectronInHomogeneousFieldsTests() override = default;
+};
---
+++ tests/integration_tests/wilsoncloudchamber/WilsonCloudChamberTests.h
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+class WilsonCloudChamberTests : public ::testing::Test
+{
+  protected:
+    WilsonCloudChamberTests() = default;
+    virtual ~WilsonCloudChamberTests() override = default;
+};
