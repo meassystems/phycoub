3b78dfaad5d2389f2da75fc26916aeb8d2bfce04
Sergey Frantsishkov <mgistrser@gmail.com>
2021-01-01 15:04:17 +0300

    Remove CTestTestfile.cmake

--- tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/franz/meassystems/phycoub/tests
-# Build directory: /Users/franz/meassystems/phycoub/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("unit_tests")
--- tests/unit_tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/franz/meassystems/phycoub/tests/unit_tests
-# Build directory: /Users/franz/meassystems/phycoub/tests/unit_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("borderconditions")
