c87604beec83da81dee72caee28d7294df065cf5
Sergey Frantsishkov <mgistrser@gmail.com>
2021-01-06 22:35:27 +0300

    fieldVectorsInCube research

--- octave/fieldVectorsInCube.m
+++ octave/fieldVectorsInCube.m
@@ -1,55 +1,100 @@
 1;
 
-function [v, u] = calculatePlaneBasis (planeNorm)
+% Построение параллельных векторов в выбранном направлении,
+% для демонстрации полей непосредственно в моделируемом пространстве
+
+function [v, u] = calculatePlaneBasis (n)
+    % Вычисление базиса плоскости заданной нормальным вектором
     v = rand(1, 3) .+ 1;
-    if (planeNorm(1) ~= 0)
-        v(1) = -1 * (planeNorm(2) * v(2) + planeNorm(3) * v(3)) / planeNorm(1);
-    elseif (planeNorm(2) ~= 0)
-        v(2) = -1 * (planeNorm(1) * v(1) + planeNorm(3) * v(3)) / planeNorm(2);
-    elseif (planeNorm(3) ~= 0)
-        v(3) = -1 * (planeNorm(1) * v(1) + planeNorm(2) * v(2)) / planeNorm(3);
+    if (n(1) ~= 0)
+        v(1) = -1 * (n(2) * v(2) + n(3) * v(3)) / n(1);
+    elseif (n(2) ~= 0)
+        v(2) = -1 * (n(1) * v(1) + n(3) * v(3)) / n(2);
+    elseif (n(3) ~= 0)
+        v(3) = -1 * (n(1) * v(1) + n(2) * v(2)) / n(3);
     endif
     v = v ./ norm(v);
 
-    uHelper = [planeNorm(1) - v(1), planeNorm(2) - v(2), planeNorm(3) - v(3)];
-    u = rand(1, 3) .+ 1;
-    if (uHelper(1) ~= 0)
-        u(1) = -1 * (uHelper(2) * u(2) + uHelper(3) * u(3)) / uHelper(1);
-    elseif (uHelper(2) ~= 0)
-        u(2) = -1 * (uHelper(1) * u(1) + uHelper(3) * u(3)) / uHelper(2);
-    elseif (uHelper(3) ~= 0)
-        u(3) = -1 * (uHelper(1) * u(1) + uHelper(2) * u(2)) / uHelper(3);
+    nNotNullIndex = find(n(:) > 0, 1);
+    vNotNullIndex = find(v(:) > 0, 1);
+    if (vNotNullIndex == nNotNullIndex)
+        vNotNullIndex = find(v(vNotNullIndex + 1:end) > 0, 1) + vNotNullIndex;
     endif
+    uRandIndex = 1 + 2 + 3 - nNotNullIndex - vNotNullIndex;
+
+    nvCoeff = v(nNotNullIndex) / (n(nNotNullIndex) * v(vNotNullIndex));
+
+    u = rand(1, 3) .+ 1;
+    u(vNotNullIndex) = u(uRandIndex) * (n(uRandIndex) * nvCoeff - v(uRandIndex) / v(vNotNullIndex)) / ( 1 - n(vNotNullIndex) * nvCoeff );
+    u(nNotNullIndex) = -1 * (u(vNotNullIndex) * n(vNotNullIndex) + u(uRandIndex) * n(uRandIndex)) / n(nNotNullIndex);
     u = u ./ norm(u);
 endfunction
 
-directionColor = 'blue';
+function [v, u, n] = calculateSpaceBasis(direction)
+    n = direction ./ norm(direction);
+    [v, u] = calculatePlaneBasis(n);
+endfunction
 
-direction = [0, 0, 1];
-normDirection = direction ./ norm(direction);
-directionQuiver = quiver3(0, 0, 0, normDirection(1), normDirection(2), normDirection(3));
-set(directionQuiver, 'Color', directionColor);
+function [fieldVectorsBegin, fieldVectorsEnd] = calculateFieldVectors(v, u, n, spaceWhd)
+    fieldVectorsBegin = [];
+    fieldVectorsEnd = [];
+
+    center = spaceWhd ./ 2;
+    maxLinearSize = max(spaceWhd);
+    scaleCoef = 2 * maxLinearSize; %  / norm(n) = 1
+
+    numCountPoitPerDirection = 10;
+    step = scaleCoef / numCountPoitPerDirection;
+
+    vIterator = center - n .* scaleCoef ./ 2 .- v .* scaleCoef ./ 2 .- u .* scaleCoef ./ 2
+    for i = 1:numCountPoitPerDirection + 1
+        uIterator = vIterator;
+        for j = 1:numCountPoitPerDirection + 1
+            fieldVectorsBegin = [fieldVectorsBegin; uIterator];
+            fieldVectorsEnd = [fieldVectorsEnd; uIterator + n .* scaleCoef];
+
+            uIterator = uIterator + u .* step;
+        endfor
+        vIterator = vIterator + v .* step;
+    endfor
+
+endfunction
+
+function drawDirectionAndPlaneBasis (v, u ,n)
+    directionColor = 'blue';
+
+    directionQuiver = quiver3(0, 0, 0, n(1), n(2), n(3));
+    set(directionQuiver, 'Color', directionColor);
+    hold on;
+
+    scatter3([0], [0], [0]);
+    hold on;
+
+    vQuiver = quiver3(0, 0, 0, v(1), v(2), v(3));
+    set(vQuiver, 'Color', 'Red');
+    hold on;
+
+    uQuiver = quiver3(0, 0, 0, u(1), u(2), u(3));
+    set(uQuiver, 'Color', 'Green');
+    hold on;
+endfunction
+
+function drawField (fieldVectorsBegin, fieldVectorsEnd)
+    for i = 1:1:length(fieldVectorsBegin)
+        plot3([fieldVectorsBegin(i, 1), fieldVectorsEnd(i, 1)], [fieldVectorsBegin(i, 2), fieldVectorsEnd(i, 2)], [fieldVectorsBegin(i, 3), fieldVectorsEnd(i, 3)], 'm');
+        hold on;
+    endfor
+endfunction
+
+[x, y, z] = sphere (40);
+surf (x * 0.5 + 0.5, y * 0.5 + 0.5, z * 0.5 + 0.5);
 hold on;
 
-directionScatter = scatter3([0], [0], [0]);
-hold on;
+direction = [1, 2, 3];
+[v, u, n] = calculateSpaceBasis(direction);
 
-[v, u] = calculatePlaneBasis(normDirection)
-vQuiver = quiver3(0, 0, 0, v(1), v(2), v(3));
-set(vQuiver, 'Color', 'Red');
-hold on;
+drawDirectionAndPlaneBasis(v, u, n);
 
-uQuiver = quiver3(0, 0, 0, u(1), u(2), u(3));
-set(uQuiver, 'Color', 'Green');
-hold on;
-
-%set(quiversensorx, 'LineWidth', 1);
-%set(quiversensory, 'LineWidth', 1);
-%set(quiversensorz, 'LineWidth', 1);
-%
-%axis equal
-%
-%textx = text(5, 0, 1, 'This is the x-vector')
-%set(textx, 'Color', 'blue')
-
-hold off;
+cubeWhd = [1, 1, 1];
+[fieldVectorsBegin, fieldVectorsEnd] = calculateFieldVectors(v, u, n, cubeWhd)
+drawField(fieldVectorsBegin, fieldVectorsEnd);
