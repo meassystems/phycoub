61268f04d95123a7f3c444d2b3faccc345876387
github-action-benchmark <github@users.noreply.github.com>
2021-01-08 21:14:48 +0000

    add Benchmark (googlecpp) benchmark result for 55889bbecb245e9669f3826b8c108ec936050f8f

---
+++ dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610140488893,
+  "repoUrl": "https://github.com/meassystems/phycoub",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mgistrser@gmail.com",
+            "name": "Sergey Frantsishkov",
+            "username": "magistrser"
+          },
+          "committer": {
+            "email": "mgistrser@gmail.com",
+            "name": "Sergey Frantsishkov",
+            "username": "magistrser"
+          },
+          "distinct": true,
+          "id": "55889bbecb245e9669f3826b8c108ec936050f8f",
+          "message": "Bm try 3",
+          "timestamp": "2021-01-09T00:05:23+03:00",
+          "tree_id": "783e37c725f77b432cb3a55546ddb4675a0cff2d",
+          "url": "https://github.com/meassystems/phycoub/commit/55889bbecb245e9669f3826b8c108ec936050f8f"
+        },
+        "date": 1610140487596,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_100Particle",
+            "value": 932470.0498687255,
+            "unit": "ns/iter",
+            "extra": "iterations: 762\ncpu: 930905.5118110235 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_500Particle",
+            "value": 22746089.647435714,
+            "unit": "ns/iter",
+            "extra": "iterations: 156\ncpu: 3655525.64102564 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_1000Particle",
+            "value": 86530227.73831755,
+            "unit": "ns/iter",
+            "extra": "iterations: 107\ncpu: 6302289.7196261715 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_2500Particle",
+            "value": 500598140.19999754,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 48330999.999999985 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_5000Particle",
+            "value": 2111132276.1999984,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 275220600.00000006 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
