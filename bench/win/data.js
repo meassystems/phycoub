29bb5dc5fd88da3cd408361d783040a918e9fdcd
github-action-benchmark <github@users.noreply.github.com>
2021-01-09 09:34:35 +0000

    add Benchmark (googlecpp) benchmark result for f0f5fc711f28604d60f54727b94733d0bfb016c9

---
+++ bench/win/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1610184875492,
+  "repoUrl": "https://github.com/meassystems/phycoub",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mgistrser@gmail.com",
+            "name": "Sergey Frantsishkov",
+            "username": "magistrser"
+          },
+          "committer": {
+            "email": "mgistrser@gmail.com",
+            "name": "Sergey Frantsishkov",
+            "username": "magistrser"
+          },
+          "distinct": true,
+          "id": "f0f5fc711f28604d60f54727b94733d0bfb016c9",
+          "message": "Remove root /",
+          "timestamp": "2021-01-09T12:24:50+03:00",
+          "tree_id": "580b6b58ce1f1805340c298d01a2591fe2196c2a",
+          "url": "https://github.com/meassystems/phycoub/commit/f0f5fc711f28604d60f54727b94733d0bfb016c9"
+        },
+        "date": 1610184873195,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Magnetron_100Particle",
+            "value": 1823988.471850091,
+            "unit": "ns/iter",
+            "extra": "iterations: 373\ncpu: 1801273.4584450403 ns\nthreads: 1"
+          },
+          {
+            "name": "Magnetron_500Particle",
+            "value": 259267370.00000912,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 160937500 ns\nthreads: 1"
+          },
+          {
+            "name": "Magnetron_1000Particle",
+            "value": 643753059.9999945,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 454687500 ns\nthreads: 1"
+          },
+          {
+            "name": "Magnetron_2500Particle",
+            "value": 3718293500.000005,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2767187500 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
