2d7f610a7325a648a86478f4e762fab3cb23dd46
github-action-benchmark <github@users.noreply.github.com>
2021-01-09 01:06:36 +0000

    add Benchmark (googlecpp) benchmark result for 66b03ebf0a64099da8c830d1b01331cb563a08b9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610154145462,
+  "lastUpdate": 1610154396237,
   "repoUrl": "https://github.com/meassystems/phycoub",
   "entries": {
     "Benchmark": [
@@ -144,6 +144,54 @@ window.BENCHMARK_DATA = {
             "value": 516910073.500003,
             "unit": "ns/iter",
             "extra": "iterations: 10\ncpu: 55964300.00000004 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mgistrser@gmail.com",
+            "name": "Sergey Frantsishkov",
+            "username": "magistrser"
+          },
+          "committer": {
+            "email": "mgistrser@gmail.com",
+            "name": "Sergey Frantsishkov",
+            "username": "magistrser"
+          },
+          "distinct": true,
+          "id": "66b03ebf0a64099da8c830d1b01331cb563a08b9",
+          "message": "Different plans for diff banch",
+          "timestamp": "2021-01-09T03:47:04+03:00",
+          "tree_id": "e7d480f77c7baa3f8f2eae058456b453bc1ce69d",
+          "url": "https://github.com/meassystems/phycoub/commit/66b03ebf0a64099da8c830d1b01331cb563a08b9"
+        },
+        "date": 1610154392972,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Magnetron_100Particle",
+            "value": 1691889.4348896006,
+            "unit": "ns/iter",
+            "extra": "iterations: 407\ncpu: 1650798.525798526 ns\nthreads: 1"
+          },
+          {
+            "name": "Magnetron_500Particle",
+            "value": 256682559.99999017,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 157812500 ns\nthreads: 1"
+          },
+          {
+            "name": "Magnetron_1000Particle",
+            "value": 698197829.9999809,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 514062500 ns\nthreads: 1"
+          },
+          {
+            "name": "Magnetron_2500Particle",
+            "value": 3279500319.999988,
+            "unit": "ns/iter",
+            "extra": "iterations: 10\ncpu: 2404687500 ns\nthreads: 1"
           }
         ]
       }
